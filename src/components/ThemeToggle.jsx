fb93214757e83a3216d86d7c157005f807285540
it <kyle9742@naver.com>
2024-06-17 14:37:46 +0900

    테마 다크 라이트

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,11 +1,18 @@
-
+import { useGlobalContext } from "../context/context";
+import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 
 const ThemeToggle = () => {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
   return (
-    <div>
-      테마색
-    </div>
-  )
-}
-
-export default ThemeToggle
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillMoonFill className="toggle-icon" />
+        ) : (
+          <BsFillSunFill className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
+};
+export default ThemeToggle;
